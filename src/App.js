4365a5d1456701453bdc08925c86fc62f926ca6d
kirilltimofeev002 <timofeev2002252@gmail.com>
2024-12-12 20:19:28 +0900

    чето сделал день 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const firstCount = 50; // начальное число
+  const [count, setCount] = useState(firstCount);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" id="App">
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount(count * 2)}>*2</button>
+        <button onClick={() => setCount(count / 2)}>/2</button>
+        <button onClick={() => setCount(count * count)}>^2</button>
+        <button onClick={() => setCount(firstCount)}>обновить</button>
+        <h1>Счётчик: {count}</h1>
+      </div>
+
+      
   );
 }
 
